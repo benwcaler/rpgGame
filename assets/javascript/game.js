9a6be6a22e64b973966cd61f9de5295ba5b3e940
Ben <b.wayne@me.com>
2018-02-07 11:24:31 -0700

    images, html, and css in the works

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    playerSelected = false;
+    attackPower = 0;
+    health = 0;
+
+    $(".char").on("click", function() {
+        playerSelected = true;
+        $(this).
+    })
+})
